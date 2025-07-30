158618f70cabdea9a62c49c017cac9fd2ec48609
Alfred Millett-Sikking <amsikking@gmail.com>
2025-07-30 12:39:55 -0700

    Update pco_pixelfly_usb.py
    Added the image number to the buffer timeout exception to help debug whether frames are lost at the beginning, middle or end of a multi-image call to 'record_to_memory'.

--- pco_pixelfly_usb.py
+++ pco_pixelfly_usb.py
@@ -448,7 +448,7 @@ class Camera:
             try:
                 dll.wait_for_buffer(self.handle, 1, buflist, self.timeout_ms)
             except Exception as e:
-                print("%s: -> buffer timeout?"%self.name)
+                print("%s: -> buffer timeout? (img=%i)"%(self.name, i))
                 raise
             assert buflist[0].dwStatusDll == 0xe0008000 # buffer event set
             if buflist[0].dwStatusDrv != 0: # image transfer failed
